abf4a150508808859c0408b14f76679939e71d1c
Total Profit <eperezaecm@gmail.com>
2025-11-29 12:30:55 -0700

    Refactor Firebase initialization and remove analytics
    Signed-off-by: Total Profit <eperezaecm@gmail.com>

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,21 @@
-// firebase.js
+// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
 
-// Configuración de tu Firebase (usa tu API Key y datos reales)
+// Configuración Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyB2nO5sFxNy_-Usg29WcXvk1A676Nt-lhw",
   authDomain: "financemaster-pr0.firebaseapp.com",
   projectId: "financemaster-pr0",
   storageBucket: "financemaster-pr0.appspot.com",
   messagingSenderId: "535862107904",
-  appId: "1:535862107904:web:5cc701b94d1084091ddde5",
-  measurementId: "G-8MFS6YS282"
+  appId: "1:535862107904:web:5cc701b94d1084091ddde5"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
