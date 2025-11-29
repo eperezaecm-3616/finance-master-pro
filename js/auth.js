30c1f7a596137649f370e5ca3ff6df97ba12a517
Total Profit <eperezaecm@gmail.com>
2025-11-29 12:31:15 -0700

    Refactor auth functions to use async/await
    Refactor authentication functions to use async/await for better error handling and navigation.
    
    Signed-off-by: Total Profit <eperezaecm@gmail.com>

--- js/auth.js
+++ js/auth.js
@@ -1,26 +1,30 @@
-// auth.js
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-export const registerUser = (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      alert("Registro exitoso");
-      window.location.reload();
-    })
-    .catch(error => alert(error.message));
+export const registerUser = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Registro exitoso");
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
 };
 
-export const loginUser = (email, password) => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      window.location.reload();
-    })
-    .catch(error => alert(error.message));
+export const loginUser = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
 };
 
-export const logoutUser = () => {
-  signOut(auth)
-    .then(() => window.location.reload())
-    .catch(error => alert(error.message));
+export const logoutUser = async () => {
+  try {
+    await signOut(auth);
+    window.location.href = "index.html";
+  } catch (error) {
+    alert(error.message);
+  }
 };
